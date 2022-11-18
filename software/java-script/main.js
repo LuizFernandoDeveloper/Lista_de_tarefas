2951fedea199ee499317408c5cada30377ff9b96
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-11-18 18:11:14 -0300

    update

--- software/java-script/main.js
+++ software/java-script/main.js
@@ -1,29 +1,25 @@
+
 $(document).ready(function(){
-    let idBtn = 0
-    $(document).on('click', '#terminar', function(){
-        let valor = $('#terminar').val()
-        let line = '#line' + valor;
-        $(line).remove();        
-    })
+
+    remover = function(element){
+        element.parentElement.remove();
+    }
+
     $('#button').click(function(e){
         e.preventDefault();
-
         let tarefa  = $('#tarefa').val();
-
         if(tarefa == ''){
             alert('Digite uma Tarefa');
             return;
         };
-
-        const newLine = $(`<div id="line${idBtn}"class="barra-conteudo">
+        const newLine = $(`<div class="barra-conteudo">
                                 <div>
                                     <span>${tarefa}</span>
                                 </div>
-                                <button id="terminar" onclick="remover(this)" value="${idBtn}">Terminar</button> 
+                                <button  onclick="remover(this)">Terminar</button> 
                             </div>`);
         $(newLine).appendTo('.conteudo');
         $('#tarefa').val('');
-        idBtn ++;
+        
     });
-
 });
